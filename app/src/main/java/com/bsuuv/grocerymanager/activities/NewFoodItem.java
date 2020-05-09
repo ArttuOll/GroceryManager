c48bf341798da97fe5e09481639505df45cb8381
ArttuO <arttu.olli@tutanota.com>
2020-05-09 17:05:42 +0300

    Created logic for NewFoodItem's togglebuttons.

--- app/src/main/java/com/bsuuv/grocerymanager/activities/NewFoodItem.java
+++ app/src/main/java/com/bsuuv/grocerymanager/activities/NewFoodItem.java
@@ -2,12 +2,45 @@ package com.bsuuv.grocerymanager.activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bsuuv.grocerymanager.R;
 
 public class NewFoodItem extends AppCompatActivity {
+
+    private ToggleButton weeklyToggle;
+    private ToggleButton biweeklyToggle;
+    private ToggleButton monthlyToggle;
+    private View.OnClickListener mOnToggleButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.togglebuton_biweekly && biweeklyToggle.isChecked()) {
+                weeklyToggle.setEnabled(false);
+                monthlyToggle.setEnabled(false);
+            } else if (v.getId() == R.id.togglebuton_biweekly && !biweeklyToggle.isChecked()) {
+                weeklyToggle.setEnabled(true);
+                monthlyToggle.setEnabled(true);
+            }
+
+            if (v.getId() == R.id.togglebuton_weekly && weeklyToggle.isChecked()) {
+                biweeklyToggle.setEnabled(false);
+                monthlyToggle.setEnabled(false);
+            } else if (v.getId() == R.id.togglebuton_weekly && !weeklyToggle.isChecked()) {
+                biweeklyToggle.setEnabled(true);
+                monthlyToggle.setEnabled(true);
+            }
+
+            if (v.getId() == R.id.togglebuton_monthly && monthlyToggle.isChecked()) {
+                biweeklyToggle.setEnabled(false);
+                weeklyToggle.setEnabled(false);
+            } else if (v.getId() == R.id.togglebuton_monthly && !monthlyToggle.isChecked()) {
+                biweeklyToggle.setEnabled(true);
+                weeklyToggle.setEnabled(true);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +49,28 @@ public class NewFoodItem extends AppCompatActivity {
 
         setTitle("Add Food-item");
 
+        setUpToggleButtons();
+    }
 
+    private void setUpToggleButtons() {
+        this.weeklyToggle = findViewById(R.id.togglebuton_weekly);
+        this.biweeklyToggle = findViewById(R.id.togglebuton_biweekly);
+        this.monthlyToggle = findViewById(R.id.togglebuton_monthly);
+
+        biweeklyToggle.setText(R.string.togglebutton_biweekly);
+        biweeklyToggle.setTextOff("Biweekly");
+        biweeklyToggle.setTextOn("Biweekly");
+        biweeklyToggle.setOnClickListener(mOnToggleButtonClickListener);
+
+        weeklyToggle.setText(R.string.togglebutton_weekly);
+        weeklyToggle.setTextOff("Weekly");
+        weeklyToggle.setTextOn("Weekly");
+        weeklyToggle.setOnClickListener(mOnToggleButtonClickListener);
+
+        monthlyToggle.setText(R.string.togglebutton_monthly);
+        monthlyToggle.setTextOff("Monthly");
+        monthlyToggle.setTextOn("Monthly");
+        monthlyToggle.setOnClickListener(mOnToggleButtonClickListener);
     }
 
     public void onFabClick(View view) {
