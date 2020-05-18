039daef34bc84ae7c9fb1e9e16c56943e7f3b68e
ArttuO <arttu.olli@tutanota.com>
2020-05-18 18:16:15 +0300

    NewFoodItem now uses Glide to put image into imageview.

--- app/src/main/java/com/bsuuv/grocerymanager/activities/NewFoodItem.java
+++ app/src/main/java/com/bsuuv/grocerymanager/activities/NewFoodItem.java
@@ -1,8 +1,6 @@
 package com.bsuuv.grocerymanager.activities;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -18,6 +16,7 @@ import androidx.core.content.FileProvider;
 
 import com.bsuuv.grocerymanager.R;
 import com.bsuuv.grocerymanager.domain.FoodItem;
+import com.bumptech.glide.Glide;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,44 +27,44 @@ public class NewFoodItem extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    private ToggleButton weeklyToggle;
-    private ToggleButton biweeklyToggle;
-    private ToggleButton monthlyToggle;
+    private ToggleButton mWeeklyToggle;
+    private ToggleButton mBiweeklyToggle;
+    private ToggleButton mMonthlyToggle;
     private View.OnClickListener mOnToggleButtonClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (v.getId() == R.id.togglebuton_biweekly && biweeklyToggle.isChecked()) {
-                weeklyToggle.setEnabled(false);
-                monthlyToggle.setEnabled(false);
-            } else if (v.getId() == R.id.togglebuton_biweekly && !biweeklyToggle.isChecked()) {
-                weeklyToggle.setEnabled(true);
-                monthlyToggle.setEnabled(true);
+            if (v.getId() == R.id.togglebuton_biweekly && mBiweeklyToggle.isChecked()) {
+                mWeeklyToggle.setEnabled(false);
+                mMonthlyToggle.setEnabled(false);
+            } else if (v.getId() == R.id.togglebuton_biweekly && !mBiweeklyToggle.isChecked()) {
+                mWeeklyToggle.setEnabled(true);
+                mMonthlyToggle.setEnabled(true);
             }
 
-            if (v.getId() == R.id.togglebuton_weekly && weeklyToggle.isChecked()) {
-                biweeklyToggle.setEnabled(false);
-                monthlyToggle.setEnabled(false);
-            } else if (v.getId() == R.id.togglebuton_weekly && !weeklyToggle.isChecked()) {
-                biweeklyToggle.setEnabled(true);
-                monthlyToggle.setEnabled(true);
+            if (v.getId() == R.id.togglebuton_weekly && mWeeklyToggle.isChecked()) {
+                mBiweeklyToggle.setEnabled(false);
+                mMonthlyToggle.setEnabled(false);
+            } else if (v.getId() == R.id.togglebuton_weekly && !mWeeklyToggle.isChecked()) {
+                mBiweeklyToggle.setEnabled(true);
+                mMonthlyToggle.setEnabled(true);
             }
 
-            if (v.getId() == R.id.togglebuton_monthly && monthlyToggle.isChecked()) {
-                biweeklyToggle.setEnabled(false);
-                weeklyToggle.setEnabled(false);
-            } else if (v.getId() == R.id.togglebuton_monthly && !monthlyToggle.isChecked()) {
-                biweeklyToggle.setEnabled(true);
-                weeklyToggle.setEnabled(true);
+            if (v.getId() == R.id.togglebuton_monthly && mMonthlyToggle.isChecked()) {
+                mBiweeklyToggle.setEnabled(false);
+                mWeeklyToggle.setEnabled(false);
+            } else if (v.getId() == R.id.togglebuton_monthly && !mMonthlyToggle.isChecked()) {
+                mBiweeklyToggle.setEnabled(true);
+                mWeeklyToggle.setEnabled(true);
             }
         }
     };
 
-    private EditText labelEditText;
-    private EditText brandEditText;
-    private EditText amountEditText;
-    private EditText infoEditText;
-    private ImageView foodImageView;
-    private Uri currentPhotoPath;
+    private EditText mLabelEditText;
+    private EditText mBrandEditText;
+    private EditText mAmountEditText;
+    private EditText mInfoEditText;
+    private ImageView mFoodImageView;
+    private Uri mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,20 +73,20 @@ public class NewFoodItem extends AppCompatActivity {
 
         setTitle("Add Food-item");
 
-        this.labelEditText = findViewById(R.id.editText_label);
-        this.brandEditText = findViewById(R.id.editText_brand);
-        this.amountEditText = findViewById(R.id.editText_amount);
-        this.infoEditText = findViewById(R.id.editText_info);
-        this.foodImageView = findViewById(R.id.imageView_new_fooditem);
+        this.mLabelEditText = findViewById(R.id.editText_label);
+        this.mBrandEditText = findViewById(R.id.editText_brand);
+        this.mAmountEditText = findViewById(R.id.editText_amount);
+        this.mInfoEditText = findViewById(R.id.editText_info);
+        this.mFoodImageView = findViewById(R.id.imageView_new_fooditem);
 
         setUpToggleButtons();
     }
 
     public void onFabClick(View view) {
-        String label = labelEditText.getText().toString();
-        String brand = brandEditText.getText().toString();
-        String amount = amountEditText.getText().toString();
-        String info = infoEditText.getText().toString();
+        String label = mLabelEditText.getText().toString();
+        String brand = mBrandEditText.getText().toString();
+        String amount = mAmountEditText.getText().toString();
+        String info = mInfoEditText.getText().toString();
         int frequency = getActiveToggleButton();
 
         Intent toConfigs = new Intent(this, Configurations.class);
@@ -96,7 +95,7 @@ public class NewFoodItem extends AppCompatActivity {
         toConfigs.putExtra("amount", amount);
         toConfigs.putExtra("info", info);
         toConfigs.putExtra("frequency", frequency);
-        toConfigs.putExtra("Uri", currentPhotoPath);
+        toConfigs.putExtra("Uri", mCurrentPhotoPath);
 
         setResult(RESULT_OK, toConfigs);
         finish();
@@ -127,40 +126,40 @@ public class NewFoodItem extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE) {
             if (resultCode == RESULT_OK) {
                 if (data != null) {
-                    // TODO: muuta käyttämään Glidea kuvan asettamiseen
-                    Bitmap foodImage = BitmapFactory.decodeFile(currentPhotoPath.getPath());
-                    foodImageView.setImageBitmap(foodImage);
+                    Glide.with(this)
+                            .load(new File(String.valueOf(mCurrentPhotoPath.getPath())))
+                            .into(mFoodImageView);
                 }
             }
         }
     }
 
     private int getActiveToggleButton() {
-        if (biweeklyToggle.isChecked()) return FoodItem.Frequency.BIWEEKLY;
-        else if (weeklyToggle.isChecked()) return FoodItem.Frequency.WEEKLY;
-        else if (monthlyToggle.isChecked()) return FoodItem.Frequency.MONTHLY;
+        if (mBiweeklyToggle.isChecked()) return FoodItem.Frequency.BIWEEKLY;
+        else if (mWeeklyToggle.isChecked()) return FoodItem.Frequency.WEEKLY;
+        else if (mMonthlyToggle.isChecked()) return FoodItem.Frequency.MONTHLY;
         else return -1;
     }
 
     private void setUpToggleButtons() {
-        this.weeklyToggle = findViewById(R.id.togglebuton_weekly);
-        this.biweeklyToggle = findViewById(R.id.togglebuton_biweekly);
-        this.monthlyToggle = findViewById(R.id.togglebuton_monthly);
+        this.mWeeklyToggle = findViewById(R.id.togglebuton_weekly);
+        this.mBiweeklyToggle = findViewById(R.id.togglebuton_biweekly);
+        this.mMonthlyToggle = findViewById(R.id.togglebuton_monthly);
 
-        biweeklyToggle.setText(R.string.togglebutton_biweekly);
-        biweeklyToggle.setTextOff("Biweekly");
-        biweeklyToggle.setTextOn("Biweekly");
-        biweeklyToggle.setOnClickListener(mOnToggleButtonClickListener);
+        mBiweeklyToggle.setText(R.string.togglebutton_biweekly);
+        mBiweeklyToggle.setTextOff("Biweekly");
+        mBiweeklyToggle.setTextOn("Biweekly");
+        mBiweeklyToggle.setOnClickListener(mOnToggleButtonClickListener);
 
-        weeklyToggle.setText(R.string.togglebutton_weekly);
-        weeklyToggle.setTextOff("Weekly");
-        weeklyToggle.setTextOn("Weekly");
-        weeklyToggle.setOnClickListener(mOnToggleButtonClickListener);
+        mWeeklyToggle.setText(R.string.togglebutton_weekly);
+        mWeeklyToggle.setTextOff("Weekly");
+        mWeeklyToggle.setTextOn("Weekly");
+        mWeeklyToggle.setOnClickListener(mOnToggleButtonClickListener);
 
-        monthlyToggle.setText(R.string.togglebutton_monthly);
-        monthlyToggle.setTextOff("Monthly");
-        monthlyToggle.setTextOn("Monthly");
-        monthlyToggle.setOnClickListener(mOnToggleButtonClickListener);
+        mMonthlyToggle.setText(R.string.togglebutton_monthly);
+        mMonthlyToggle.setTextOff("Monthly");
+        mMonthlyToggle.setTextOn("Monthly");
+        mMonthlyToggle.setOnClickListener(mOnToggleButtonClickListener);
     }
 
     private File createImageFile() throws IOException {
@@ -169,7 +168,7 @@ public class NewFoodItem extends AppCompatActivity {
         File storegeDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(imageFileName, ".jpg", storegeDir);
 
-        currentPhotoPath = Uri.parse(image.toURI().getPath());
+        mCurrentPhotoPath = Uri.parse(image.toURI().getPath());
 
         return image;
     }
