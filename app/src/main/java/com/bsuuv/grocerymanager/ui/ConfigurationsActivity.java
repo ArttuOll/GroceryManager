9c7d5445cfd96f89cfc90ec47239f772c9586479
ArttuOll <arttuoll.tech@tutanota.com>
2020-08-10 19:10:55 +0300

    Refactor ConfigurationsActivity

--- app/src/main/java/com/bsuuv/grocerymanager/ui/ConfigurationsActivity.java
+++ app/src/main/java/com/bsuuv/grocerymanager/ui/ConfigurationsActivity.java
@@ -28,7 +28,9 @@ import java.util.Objects;
 public class ConfigurationsActivity extends AppCompatActivity {
 
     public static final int FOOD_ITEM_EDIT_REQUEST = 2;
+
     private static final int FOOD_ITEM_CREATE_REQUEST = 1;
+
     private ConfigurationsListAdapter mAdapter;
     private FoodItemViewModel mFoodItemViewModel;
     private RecyclerView mRecyclerView;
@@ -38,86 +40,28 @@ public class ConfigurationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configurations);
-        setTitle("ConfigurationsActivity");
-
-        this.mRecyclerViewPlaceHolder = findViewById(R.id.config_recyclerview_placeholder);
-        setUpRecyclerView();
-
+        initMembers();
+        configureUi();
         setUpViewModel();
-
-        // Manages dragging and swiping items in mRecyclerView
-        ItemTouchHelper helper = initializeItemTouchHelper();
-        helper.attachToRecyclerView(mRecyclerView);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    @Nullable Intent fromNewFoodItem) {
-        super.onActivityResult(requestCode, resultCode, fromNewFoodItem);
-
-        // Result when NewFoodItem was launched to create a new food-item using the FAB.
-        if (requestCode == FOOD_ITEM_CREATE_REQUEST && resultCode == RESULT_OK && fromNewFoodItem != null) {
-            FoodItemEntity result = createFoodItemFromIntent(fromNewFoodItem);
-            mFoodItemViewModel.insert(result);
-            // Result when NewFoodItem was launched to edit a food-item by clicking one in the
-            // RecyclerView.
-        } else if (requestCode == FOOD_ITEM_EDIT_REQUEST) {
-            if (resultCode == RESULT_OK && fromNewFoodItem != null) {
-                FoodItemEntity result = updateFoodItemByIntent(fromNewFoodItem);
-                mFoodItemViewModel.update(result);
-            }
-        }
+    private void initMembers() {
+        this.mRecyclerViewPlaceHolder = findViewById(R.id.config_recyclerview_placeholder);
+        this.mFoodItemViewModel = new ViewModelProvider(this).get(FoodItemViewModel.class);
+        this.mRecyclerView = findViewById(R.id.config_recyclerview);
+        this.mAdapter = new ConfigurationsListAdapter(this);
     }
 
-    /**
-     * Called when the floating action button in this activity is pressed. Launches
-     * <code>NewFoodItemActivity</code> for creating a new <code>FoodItem</code>.
-     *
-     * @param view The view that has been clicked, in this case, the FAB.
-     *             Default parameter required by the system.
-     */
-    public void onFabClick(View view) {
-        Intent toNewFoodItem = new Intent(this, NewFoodItem.class);
-        startActivityForResult(toNewFoodItem, FOOD_ITEM_CREATE_REQUEST);
-    }
-
-    private FoodItemEntity updateFoodItemByIntent(Intent fromNewFoodItem) {
-        String label = fromNewFoodItem.getStringExtra("label");
-        String brand = fromNewFoodItem.getStringExtra("brand");
-        int amount = fromNewFoodItem.getIntExtra("amount", 0);
-        String unit = fromNewFoodItem.getStringExtra("unit");
-        String info = fromNewFoodItem.getStringExtra("info");
-        int timeFrame = fromNewFoodItem.getIntExtra("time_frame", 0);
-        int frequency = fromNewFoodItem.getIntExtra("frequency", 0);
-        String imageUri = fromNewFoodItem.getStringExtra("uri");
-        int id = fromNewFoodItem.getIntExtra("id", 0);
-        double countdownValue = fromNewFoodItem.getDoubleExtra("countdownValue", 0);
-
-        return new FoodItemEntity(id, Objects.requireNonNull(label), brand, info, amount, unit,
-                timeFrame, frequency, imageUri, countdownValue);
-    }
-
-    private FoodItemEntity createFoodItemFromIntent(Intent fromNewFoodItem) {
-        String label = fromNewFoodItem.getStringExtra("label");
-        String brand = fromNewFoodItem.getStringExtra("brand");
-        int amount = fromNewFoodItem.getIntExtra("amount", 0);
-        String unit = fromNewFoodItem.getStringExtra("unit");
-        String info = fromNewFoodItem.getStringExtra("info");
-        int timeFrame = fromNewFoodItem.getIntExtra("time_frame", 0);
-        int frequency = fromNewFoodItem.getIntExtra("frequency", 0);
-        String imageUri = fromNewFoodItem.getStringExtra("uri");
-        double initCountdownValue = fromNewFoodItem.getDoubleExtra("frequencyQuotient", 0.0);
-
-        return new FoodItemEntity(Objects.requireNonNull(label), brand, info, amount, unit,
-                timeFrame, frequency, imageUri, initCountdownValue);
+    private void configureUi() {
+        setTitle("Food-items");
+        setUpRecyclerView();
     }
 
     private void setUpRecyclerView() {
-        this.mRecyclerView = findViewById(R.id.config_recyclerview);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        this.mAdapter = new ConfigurationsListAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
+        ItemTouchHelper helper = initializeItemTouchHelper();
+        helper.attachToRecyclerView(mRecyclerView);
     }
 
     private ItemTouchHelper initializeItemTouchHelper() {
@@ -139,22 +83,98 @@ public class ConfigurationsActivity extends AppCompatActivity {
     }
 
     private void setUpViewModel() {
-        this.mFoodItemViewModel = new ViewModelProvider(this).get(FoodItemViewModel.class);
-        mFoodItemViewModel.getFoodItems().observe(this,
-                foodItemEntities -> {
-                    setRecyclerViewVisibility(foodItemEntities.size());
-                    mAdapter.setFoodItems(foodItemEntities);
-                });
+        mFoodItemViewModel.getFoodItems().observe(this, foodItemEntities -> {
+            setRecyclerViewVisibility(foodItemEntities.size());
+            mAdapter.setFoodItems(foodItemEntities);
+        });
     }
 
     private void setRecyclerViewVisibility(int size) {
         if (size > 0) {
-            mRecyclerView.setVisibility(View.VISIBLE);
+            toggleRecyclerViewVisibility(View.VISIBLE, 0);
+        } else {
+            toggleRecyclerViewVisibility(View.GONE, R.string.no_grocery_items);
+        }
+    }
+
+    private void toggleRecyclerViewVisibility(int visibility, int placeholderStrResourceId) {
+        if (visibility == View.VISIBLE) {
+            mRecyclerView.setVisibility(visibility);
             mRecyclerViewPlaceHolder.setVisibility(View.GONE);
         } else {
             mRecyclerView.setVisibility(View.GONE);
             mRecyclerViewPlaceHolder.setVisibility(View.VISIBLE);
-            mRecyclerViewPlaceHolder.setText(R.string.no_grocery_items);
+            mRecyclerViewPlaceHolder.setText(placeholderStrResourceId);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    @Nullable Intent fromNewFoodItem) {
+        super.onActivityResult(requestCode, resultCode, fromNewFoodItem);
+
+        if (foodItemCreationSuccesful(requestCode, resultCode, fromNewFoodItem)) {
+            FoodItemEntity result = createFoodItemFromIntent(fromNewFoodItem);
+            mFoodItemViewModel.insert(result);
+        } else if (foodItemEditSuccesful(requestCode, resultCode, fromNewFoodItem)) {
+            FoodItemEntity result = updateFoodItemByIntent(fromNewFoodItem);
+            mFoodItemViewModel.update(result);
+        }
+    }
+
+    private boolean foodItemCreationSuccesful(int requestCode, int resultCode,
+                                              Intent fromNewFoodItem) {
+        return requestCode == FOOD_ITEM_CREATE_REQUEST &&
+                resultCode == RESULT_OK &&
+                fromNewFoodItem != null;
+    }
+
+    private FoodItemEntity createFoodItemFromIntent(Intent fromNewFoodItem) {
+        String label = Objects.requireNonNull(fromNewFoodItem.getStringExtra("label"));
+        String brand = fromNewFoodItem.getStringExtra("brand");
+        int amount = fromNewFoodItem.getIntExtra("amount", 0);
+        String unit = fromNewFoodItem.getStringExtra("unit");
+        String info = fromNewFoodItem.getStringExtra("info");
+        int timeFrame = fromNewFoodItem.getIntExtra("time_frame", 0);
+        int frequency = fromNewFoodItem.getIntExtra("frequency", 0);
+        String imageUri = fromNewFoodItem.getStringExtra("uri");
+        double initCountdownValue = fromNewFoodItem.getDoubleExtra("frequencyQuotient", 0.0);
+
+        return new FoodItemEntity(label, brand, info, amount, unit, timeFrame, frequency, imageUri,
+                initCountdownValue);
+    }
+
+    private boolean foodItemEditSuccesful(int requestCode, int resultCode, Intent fromNewFoodItem) {
+        return requestCode == FOOD_ITEM_EDIT_REQUEST &&
+                resultCode == RESULT_OK &&
+                fromNewFoodItem != null;
+    }
+
+    private FoodItemEntity updateFoodItemByIntent(Intent fromNewFoodItem) {
+        String label = Objects.requireNonNull(fromNewFoodItem.getStringExtra("label"));
+        String brand = fromNewFoodItem.getStringExtra("brand");
+        int amount = fromNewFoodItem.getIntExtra("amount", 0);
+        String unit = fromNewFoodItem.getStringExtra("unit");
+        String info = fromNewFoodItem.getStringExtra("info");
+        int timeFrame = fromNewFoodItem.getIntExtra("time_frame", 0);
+        int frequency = fromNewFoodItem.getIntExtra("frequency", 0);
+        String imageUri = fromNewFoodItem.getStringExtra("uri");
+        int id = fromNewFoodItem.getIntExtra("id", 0);
+        double countdownValue = fromNewFoodItem.getDoubleExtra("countdownValue", 0);
+
+        return new FoodItemEntity(id, label, brand, info, amount, unit, timeFrame, frequency,
+                imageUri, countdownValue);
+    }
+
+    /**
+     * Called when the floating action button in this activity is pressed. Launches
+     * <code>NewFoodItemActivity</code> for creating a new <code>FoodItem</code>.
+     *
+     * @param view The view that has been clicked, in this case, the FAB.
+     *             Default parameter required by the system.
+     */
+    public void onFabClick(View view) {
+        Intent toNewFoodItem = new Intent(this, NewFoodItem.class);
+        startActivityForResult(toNewFoodItem, FOOD_ITEM_CREATE_REQUEST);
     }
 }
