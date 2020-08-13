0aafb9df207f6aabfaae9df9c17593b41e65e040
bsuuv <60509537+bsuuv@users.noreply.github.com>
2020-08-13 13:55:59 +0300

    Refactor ConfigurationsListAdapter (2nd pass)

--- app/src/main/java/com/bsuuv/grocerymanager/ui/adapters/ConfigurationsListAdapter.java
+++ app/src/main/java/com/bsuuv/grocerymanager/ui/adapters/ConfigurationsListAdapter.java
@@ -10,11 +10,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bsuuv.grocerymanager.R;
 import com.bsuuv.grocerymanager.db.entity.FoodItemEntity;
 import com.bsuuv.grocerymanager.ui.NewFoodItemActivity;
@@ -22,125 +20,136 @@ import com.bsuuv.grocerymanager.util.FoodItemListDifferenceCalculator;
 import com.bsuuv.grocerymanager.util.ImageViewPopulater;
 import com.bsuuv.grocerymanager.util.PluralsProvider;
 import com.bsuuv.grocerymanager.util.RequestValidator;
-
-import java.io.File;
 import java.util.List;
 
-public class ConfigurationsListAdapter extends RecyclerView.Adapter<ConfigurationsListAdapter.ConfigsViewHolder> {
+public class ConfigurationsListAdapter extends
+    RecyclerView.Adapter<ConfigurationsListAdapter.ConfigsViewHolder> {
 
-    private List<FoodItemEntity> mFoodItems;
-    private LayoutInflater mInflater;
-    private Context mContext;
+  private List<FoodItemEntity> mFoodItems;
+  private LayoutInflater mInflater;
+  private Context mContext;
 
-    public ConfigurationsListAdapter(Context context) {
-        this.mInflater = LayoutInflater.from(context);
-        this.mContext = context;
+  public ConfigurationsListAdapter(Context context) {
+    this.mInflater = LayoutInflater.from(context);
+    this.mContext = context;
+  }
+
+  @NonNull
+  @Override
+  public ConfigurationsListAdapter.ConfigsViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+      int viewType) {
+    View itemView = mInflater.inflate(R.layout.configlist_item, parent, false);
+    return new ConfigurationsListAdapter.ConfigsViewHolder(itemView, this);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull ConfigurationsListAdapter.ConfigsViewHolder holder,
+      int position) {
+    FoodItemEntity currentFoodItem = mFoodItems.get(position);
+    holder.bindTo(currentFoodItem);
+  }
+
+  @Override
+  public int getItemCount() {
+    return mFoodItems == null ? 0 : mFoodItems.size();
+  }
+
+  public FoodItemEntity getFoodItemAtPosition(int position) {
+    return mFoodItems.get(position);
+  }
+
+  public void setFoodItems(List<FoodItemEntity> newFoodItems) {
+    if (this.mFoodItems == null) {
+      initFoodItems(newFoodItems);
+    } else {
+      updateFoodItems(newFoodItems);
+    }
+  }
+
+  private void initFoodItems(List<FoodItemEntity> newFoodItems) {
+    this.mFoodItems = newFoodItems;
+    notifyItemRangeInserted(0, newFoodItems.size());
+  }
+
+  private void updateFoodItems(List<FoodItemEntity> newFoodItems) {
+    DiffUtil.DiffResult migrationOperations = FoodItemListDifferenceCalculator
+        .calculateMigrationOperations(mFoodItems, newFoodItems);
+    mFoodItems = newFoodItems;
+    migrationOperations.dispatchUpdatesTo(this);
+  }
+
+  class ConfigsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    ConfigurationsListAdapter mAdapter;
+    private TextView mFoodItemLabel, mFoodItemBrand, mFoodItemAmount, mSchedule;
+    private ImageView mFoodImage;
+    private PluralsProvider mPluralsProvider;
+
+    ConfigsViewHolder(View itemView, ConfigurationsListAdapter adapter) {
+      super(itemView);
+      itemView.setOnClickListener(this);
+      initMembers(adapter);
     }
 
-    @NonNull
+    private void initMembers(ConfigurationsListAdapter adapter) {
+      this.mFoodImage = itemView.findViewById(R.id.configlist_food_image);
+      this.mFoodImage.setClipToOutline(true);
+      this.mFoodItemLabel = itemView.findViewById(R.id.config_item_label);
+      this.mFoodItemBrand = itemView.findViewById(R.id.config_item_brand);
+      this.mFoodItemAmount = itemView.findViewById(R.id.config_item_amount);
+      this.mSchedule = itemView.findViewById(R.id.config_item_schedule);
+      this.mAdapter = adapter;
+      this.mPluralsProvider = new PluralsProvider(mContext);
+    }
+
+    void bindTo(FoodItemEntity currentFoodItem) {
+      setInputFieldValues(currentFoodItem);
+      ImageViewPopulater.populateFromUri(mContext, currentFoodItem.getImageUri(), mFoodImage);
+    }
+
+    private void setInputFieldValues(FoodItemEntity currentFoodItem) {
+      mFoodItemLabel.setText(currentFoodItem.getLabel());
+      mFoodItemBrand.setText(currentFoodItem.getBrand());
+      mFoodItemAmount.setText(mPluralsProvider.getAmountString(currentFoodItem.getAmount(),
+          currentFoodItem.getUnit()));
+      mSchedule.setText(mPluralsProvider.getScheduleString(currentFoodItem.getFrequency(),
+          currentFoodItem.getTimeFrame().value()));
+    }
+
     @Override
-    public ConfigurationsListAdapter.ConfigsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = mInflater.inflate(R.layout.configlist_item, parent, false);
-        return new ConfigurationsListAdapter.ConfigsViewHolder(itemView, this);
+    public void onClick(View v) {
+      Intent toNewFoodItem = createIntentToNewFoodItem();
+      launchConfigurationsActivity(toNewFoodItem);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ConfigurationsListAdapter.ConfigsViewHolder holder,
-                                 int position) {
-        FoodItemEntity currentFoodItem = mFoodItems.get(position);
-        holder.bindTo(currentFoodItem);
+    private void launchConfigurationsActivity(Intent toNewFoodItem) {
+      Bundle bundle = ActivityOptions.makeSceneTransitionAnimation((Activity) mContext, mFoodImage,
+          mFoodImage.getTransitionName()).toBundle();
+      Activity configurations = (Activity) mContext;
+      configurations.startActivityForResult(toNewFoodItem,
+          RequestValidator.FOOD_ITEM_EDIT_REQUEST, bundle);
     }
 
-    @Override
-    public int getItemCount() {
-        return mFoodItems == null ? 0 : mFoodItems.size();
+    private Intent createIntentToNewFoodItem() {
+      FoodItemEntity currentFoodItem = mFoodItems.get(getAdapterPosition());
+      Intent toNewFoodItem = new Intent(mContext, NewFoodItemActivity.class);
+      toNewFoodItem.putExtra("label", currentFoodItem.getLabel());
+      toNewFoodItem.putExtra("brand", currentFoodItem.getBrand());
+      toNewFoodItem.putExtra("info", currentFoodItem.getInfo());
+      toNewFoodItem.putExtra("amount", currentFoodItem.getAmount());
+      toNewFoodItem.putExtra("unit", currentFoodItem.getUnit());
+      toNewFoodItem.putExtra("time_frame", currentFoodItem.getTimeFrame());
+      toNewFoodItem.putExtra("frequency", currentFoodItem.getFrequency());
+      toNewFoodItem.putExtra("id", currentFoodItem.getId());
+      toNewFoodItem.putExtra("editPosition", getAdapterPosition());
+      toNewFoodItem.putExtra("countdownValue", currentFoodItem.getCountdownValue());
+      toNewFoodItem.putExtra("requestCode", RequestValidator.FOOD_ITEM_EDIT_REQUEST);
+      toNewFoodItem.putExtra("uri", getUri(currentFoodItem));
+      return toNewFoodItem;
     }
 
-    public FoodItemEntity getFoodItemAtPosition(int position) {
-        return mFoodItems.get(position);
+    private String getUri(FoodItemEntity foodItem) {
+      return (foodItem.getImageUri() != null) ? foodItem.getImageUri() : "";
     }
-
-    public void setFoodItems(List<FoodItemEntity> newFoodItems) {
-        if (this.mFoodItems == null) initFoodItems(newFoodItems);
-        else updateFoodItems(newFoodItems);
-    }
-
-    private void initFoodItems(List<FoodItemEntity> newFoodItems) {
-        this.mFoodItems = newFoodItems;
-        notifyItemRangeInserted(0, newFoodItems.size());
-    }
-
-    private void updateFoodItems(List<FoodItemEntity> newFoodItems) {
-        DiffUtil.DiffResult migrationOperations = FoodItemListDifferenceCalculator
-                .calculateMigrationOperations(mFoodItems, newFoodItems);
-        mFoodItems = newFoodItems;
-        migrationOperations.dispatchUpdatesTo(this);
-    }
-
-    class ConfigsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        final ConfigurationsListAdapter mAdapter;
-        private final TextView mFoodItemLabel, mFoodItemBrand, mFoodItemAmount, mSchedule;
-        private final ImageView mFoodImage;
-        private final PluralsProvider mPluralsProvider;
-
-        ConfigsViewHolder(View itemView, ConfigurationsListAdapter adapter) {
-            super(itemView);
-            mFoodImage = itemView.findViewById(R.id.configlist_food_image);
-            mFoodImage.setClipToOutline(true);
-            mFoodItemLabel = itemView.findViewById(R.id.config_item_label);
-            mFoodItemBrand = itemView.findViewById(R.id.config_item_brand);
-            mFoodItemAmount = itemView.findViewById(R.id.config_item_amount);
-            mSchedule = itemView.findViewById(R.id.config_item_schedule);
-            this.mAdapter = adapter;
-            this.mPluralsProvider = new PluralsProvider(mContext);
-            itemView.setOnClickListener(this);
-        }
-
-        void bindTo(FoodItemEntity currentFoodItem) {
-            setInputFieldValues(currentFoodItem);
-            ImageViewPopulater.populateFromUri(mContext, currentFoodItem.getImageUri(), mFoodImage);
-        }
-
-        private void setInputFieldValues(FoodItemEntity currentFoodItem) {
-            mFoodItemLabel.setText(currentFoodItem.getLabel());
-            mFoodItemBrand.setText(currentFoodItem.getBrand());
-            mFoodItemAmount.setText(mPluralsProvider.getAmountString(currentFoodItem.getAmount(),
-                                                                     currentFoodItem.getUnit()));
-            mSchedule.setText(mPluralsProvider.getScheduleString(currentFoodItem.getFrequency(),
-                                                                 currentFoodItem.getTimeFrame()
-                                                                                .value()));
-        }
-
-        @Override
-        public void onClick(View v) {
-            FoodItemEntity currentFoodItem = mFoodItems.get(getAdapterPosition());
-            Intent toNewFoodItem = createIntentToNewFoodItem(currentFoodItem);
-            Bundle bundle = ActivityOptions.makeSceneTransitionAnimation((Activity) mContext,
-                    mFoodImage, mFoodImage.getTransitionName()).toBundle();
-
-            Activity configurations = (Activity) mContext;
-            configurations.startActivityForResult(toNewFoodItem,
-                    RequestValidator.FOOD_ITEM_EDIT_REQUEST, bundle);
-        }
-
-        private Intent createIntentToNewFoodItem(FoodItemEntity foodItem) {
-            Intent toNewFoodItem = new Intent(mContext, NewFoodItemActivity.class);
-            toNewFoodItem.putExtra("label", foodItem.getLabel());
-            toNewFoodItem.putExtra("brand", foodItem.getBrand());
-            toNewFoodItem.putExtra("info", foodItem.getInfo());
-            toNewFoodItem.putExtra("amount", foodItem.getAmount());
-            toNewFoodItem.putExtra("unit", foodItem.getUnit());
-            toNewFoodItem.putExtra("time_frame", foodItem.getTimeFrame());
-            toNewFoodItem.putExtra("frequency", foodItem.getFrequency());
-            toNewFoodItem.putExtra("id", foodItem.getId());
-            toNewFoodItem.putExtra("editPosition", getAdapterPosition());
-            toNewFoodItem.putExtra("countdownValue", foodItem.getCountdownValue());
-            toNewFoodItem.putExtra("requestCode", RequestValidator.FOOD_ITEM_EDIT_REQUEST);
-
-            String uri = (foodItem.getImageUri() != null) ? foodItem.getImageUri() : "";
-            toNewFoodItem.putExtra("uri", uri);
-
-            return toNewFoodItem;
-        }
-    }
+  }
 }
