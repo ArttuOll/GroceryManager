dbdfb744a329c859b053ad25e3eac826cb558201
bsuuv <60509537+bsuuv@users.noreply.github.com>
2020-08-13 13:56:19 +0300

    Refactor GroceryListAdapter (2nd pass)

--- app/src/main/java/com/bsuuv/grocerymanager/ui/adapters/GroceryListAdapter.java
+++ app/src/main/java/com/bsuuv/grocerymanager/ui/adapters/GroceryListAdapter.java
@@ -10,12 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bsuuv.grocerymanager.R;
 import com.bsuuv.grocerymanager.db.entity.FoodItemEntity;
 import com.bsuuv.grocerymanager.ui.FoodItemDetail;
@@ -23,118 +21,130 @@ import com.bsuuv.grocerymanager.ui.FoodItemDetailFragment;
 import com.bsuuv.grocerymanager.util.FoodItemListDifferenceCalculator;
 import com.bsuuv.grocerymanager.util.ImageViewPopulater;
 import com.bsuuv.grocerymanager.util.PluralsProvider;
-
 import java.util.List;
 
 public class GroceryListAdapter extends RecyclerView.Adapter<GroceryListAdapter.GroceryViewHolder> {
 
-    private List<FoodItemEntity> mGroceryItems;
-    private LayoutInflater mInflater;
-    private Context mContext;
-    private boolean mIsWideScreen;
+  private List<FoodItemEntity> mGroceryItems;
+  private LayoutInflater mInflater;
+  private Context mContext;
+  private boolean mIsWideScreen;
 
-    public GroceryListAdapter(Context context, boolean wideScreen) {
-        this.mInflater = LayoutInflater.from(context);
-        this.mContext = context;
-        this.mIsWideScreen = wideScreen;
+  public GroceryListAdapter(Context context, boolean wideScreen) {
+    this.mInflater = LayoutInflater.from(context);
+    this.mContext = context;
+    this.mIsWideScreen = wideScreen;
+  }
+
+  @NonNull
+  @Override
+  public GroceryListAdapter.GroceryViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+      int viewType) {
+    View itemView = mInflater.inflate(R.layout.grocerylist_item, parent, false);
+    return new GroceryViewHolder(itemView, this);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull GroceryListAdapter.GroceryViewHolder holder, int position) {
+    FoodItemEntity currentFoodItem = mGroceryItems.get(position);
+    holder.bindTo(currentFoodItem);
+  }
+
+  @Override
+  public int getItemCount() {
+    return mGroceryItems == null ? 0 : mGroceryItems.size();
+  }
+
+  public void setGroceryItems(List<FoodItemEntity> newGroceryItems) {
+    if (this.mGroceryItems == null) {
+      initGroceryItems(newGroceryItems);
+    } else {
+      updateGroceryItems(newGroceryItems);
+    }
+  }
+
+  private void initGroceryItems(List<FoodItemEntity> newGroceryItems) {
+    this.mGroceryItems = newGroceryItems;
+    notifyItemRangeInserted(0, newGroceryItems.size());
+  }
+
+  private void updateGroceryItems(List<FoodItemEntity> newGroceryItems) {
+    DiffUtil.DiffResult migrationOperations = FoodItemListDifferenceCalculator
+        .calculateMigrationOperations(mGroceryItems, newGroceryItems);
+    mGroceryItems = newGroceryItems;
+    migrationOperations.dispatchUpdatesTo(this);
+  }
+
+  public FoodItemEntity getFoodItemAtPosition(int position) {
+    return mGroceryItems.get(position);
+  }
+
+  class GroceryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    GroceryListAdapter mAdapter;
+    private TextView mFoodItemLabel, mFoodItemBrand, mFoodItemAmount;
+    private PluralsProvider mPluralsProvider;
+    private ImageView mFoodImage;
+
+    GroceryViewHolder(View itemView, GroceryListAdapter adapter) {
+      super(itemView);
+      itemView.setOnClickListener(this);
+      initMembers(adapter);
     }
 
-    @NonNull
+    private void initMembers(GroceryListAdapter adapter) {
+      this.mFoodImage = itemView.findViewById(R.id.grocerylist_food_image);
+      this.mFoodImage.setClipToOutline(true);
+      this.mFoodItemLabel = itemView.findViewById(R.id.grocery_item_label);
+      this.mFoodItemBrand = itemView.findViewById(R.id.grocery_item_brand);
+      this.mFoodItemAmount = itemView.findViewById(R.id.grocery_item_amount);
+      this.mAdapter = adapter;
+      this.mPluralsProvider = new PluralsProvider(mContext);
+    }
+
+    void bindTo(FoodItemEntity currentFoodItem) {
+      setInputFieldValues(currentFoodItem);
+      ImageViewPopulater.populateFromUri(mContext, currentFoodItem.getImageUri(), mFoodImage);
+    }
+
+    private void setInputFieldValues(FoodItemEntity currentFoodItem) {
+      mFoodItemLabel.setText(currentFoodItem.getLabel());
+      mFoodItemBrand.setText(currentFoodItem.getBrand());
+      mFoodItemAmount.setText(mPluralsProvider.getAmountString(currentFoodItem.getAmount(),
+          currentFoodItem.getUnit()));
+    }
+
     @Override
-    public GroceryListAdapter.GroceryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = mInflater.inflate(R.layout.grocerylist_item, parent, false);
-        return new GroceryViewHolder(itemView, this);
+    public void onClick(View v) {
+      FoodItemEntity currentFoodItem = mGroceryItems.get(getAdapterPosition());
+      if (mIsWideScreen) {
+        showInFragment(currentFoodItem);
+      } else {
+        showInFoodItemDetailActivity(currentFoodItem);
+      }
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull GroceryListAdapter.GroceryViewHolder holder, int position) {
-        FoodItemEntity currentFoodItem = mGroceryItems.get(position);
-        holder.bindTo(currentFoodItem);
+    private void showInFragment(FoodItemEntity currentFoodItem) {
+      FoodItemDetailFragment fragment = FoodItemDetailFragment
+          .newInstance(currentFoodItem.getId());
+
+      ((FragmentActivity) mContext).getSupportFragmentManager().beginTransaction()
+          .replace(R.id.container_food_item_detail, fragment)
+          .addToBackStack(null)
+          .commit();
     }
 
-    @Override
-    public int getItemCount() {
-        return mGroceryItems == null ? 0 : mGroceryItems.size();
+    private void showInFoodItemDetailActivity(FoodItemEntity currentFoodItem) {
+      Intent toFoodItemDetail = createIntentToFoodItemDetail(currentFoodItem);
+      Bundle bundle = ActivityOptions.makeSceneTransitionAnimation((Activity) mContext,
+          mFoodImage, mFoodImage.getTransitionName()).toBundle();
+      mContext.startActivity(toFoodItemDetail, bundle);
     }
 
-    public void setGroceryItems(List<FoodItemEntity> newGroceryItems) {
-        if (this.mGroceryItems == null) initGroceryItems(newGroceryItems);
-        else updateGroceryItems(newGroceryItems);
+    private Intent createIntentToFoodItemDetail(FoodItemEntity foodItem) {
+      Intent toFoodItemDetail = new Intent(mContext, FoodItemDetail.class);
+      toFoodItemDetail.putExtra(FoodItemDetailFragment.FOOD_ITEM_ID_KEY, foodItem.getId());
+      return toFoodItemDetail;
     }
-
-    private void initGroceryItems(List<FoodItemEntity> newGroceryItems) {
-        this.mGroceryItems = newGroceryItems;
-        notifyItemRangeInserted(0, newGroceryItems.size());
-    }
-
-    private void updateGroceryItems(List<FoodItemEntity> newGroceryItems) {
-        DiffUtil.DiffResult migrationOperations = FoodItemListDifferenceCalculator
-                .calculateMigrationOperations(mGroceryItems, newGroceryItems);
-        mGroceryItems = newGroceryItems;
-        migrationOperations.dispatchUpdatesTo(this);
-    }
-
-    public FoodItemEntity getFoodItemAtPosition(int position) {
-        return mGroceryItems.get(position);
-    }
-
-    class GroceryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private final TextView mFoodItemLabel, mFoodItemBrand, mFoodItemAmount;
-        final GroceryListAdapter mAdapter;
-        private final PluralsProvider mPluralsProvider;
-        private ImageView mFoodImage;
-
-        GroceryViewHolder(View itemView, GroceryListAdapter adapter) {
-            super(itemView);
-            mFoodImage = itemView.findViewById(R.id.grocerylist_food_image);
-            mFoodImage.setClipToOutline(true);
-            mFoodItemLabel = itemView.findViewById(R.id.grocery_item_label);
-            mFoodItemBrand = itemView.findViewById(R.id.grocery_item_brand);
-            mFoodItemAmount = itemView.findViewById(R.id.grocery_item_amount);
-            this.mAdapter = adapter;
-            this.mPluralsProvider = new PluralsProvider(mContext);
-            itemView.setOnClickListener(this);
-        }
-
-        void bindTo(FoodItemEntity currentFoodItem) {
-            mFoodItemLabel.setText(currentFoodItem.getLabel());
-            mFoodItemBrand.setText(currentFoodItem.getBrand());
-            mFoodItemAmount.setText(mPluralsProvider.getAmountString(currentFoodItem.getAmount(),
-                                                                     currentFoodItem.getUnit()));
-            ImageViewPopulater.populateFromUri(mContext, currentFoodItem.getImageUri(), mFoodImage);
-        }
-
-        @Override
-        public void onClick(View v) {
-            FoodItemEntity currentFoodItem = mGroceryItems.get(getAdapterPosition());
-            if (mIsWideScreen) {
-                showInFragment(currentFoodItem);
-            } else {
-                showInFoodItemDetailActivity(currentFoodItem);
-            }
-        }
-
-        private void showInFragment(FoodItemEntity currentFoodItem) {
-            FoodItemDetailFragment fragment = FoodItemDetailFragment
-                    .newInstance(currentFoodItem.getId());
-
-            ((FragmentActivity) mContext).getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container_food_item_detail, fragment)
-                    .addToBackStack(null)
-                    .commit();
-        }
-
-        private void showInFoodItemDetailActivity(FoodItemEntity currentFoodItem) {
-            Intent toFoodItemDetail = createIntentToFoodItemDetail(currentFoodItem);
-            Bundle bundle = ActivityOptions.makeSceneTransitionAnimation((Activity) mContext,
-                    mFoodImage, mFoodImage.getTransitionName()).toBundle();
-            mContext.startActivity(toFoodItemDetail, bundle);
-        }
-
-        private Intent createIntentToFoodItemDetail(FoodItemEntity foodItem) {
-            Intent toFoodItemDetail = new Intent(mContext, FoodItemDetail.class);
-            toFoodItemDetail.putExtra(FoodItemDetailFragment.FOOD_ITEM_ID_KEY, foodItem.getId());
-            return toFoodItemDetail;
-        }
-    }
+  }
 }
